5a2dc38d1a4c20e2a982681c1258dc85fd5d321f
bit2424 <nelsonquinones2424@gmail.com>
2024-04-30 15:59:54 +0000

    :construction: Adding logging to the application

--- Backend/Transform/QueueProcessor.py
+++ Backend/Transform/QueueProcessor.py
@@ -1,9 +1,13 @@
 from Core.QueueObserver import QueueObserver 
-
+import logging
 import time
 
 
 if __name__ == "__main__":
+    
+    logging.basicConfig(filename='./Processing_Logs/transform.log', filemode='w', format='%(asctime)s %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
     observer = QueueObserver("./../Transform_Queue/")
     observer.start()
     
@@ -11,6 +15,10 @@ if __name__ == "__main__":
         # Keep the script running to monitor changes
         while True:
             observer.files_processor.update_time_to_proccess()
+            logger.info("LOL")
             time.sleep(0.5)
+    except Exception as e:
+        logger.exception("Exception occurred")
     except KeyboardInterrupt:
+        logging.info("Server Shutdown")
         observer.stop()
