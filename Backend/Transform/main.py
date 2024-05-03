434d811d01862f54cab796797cd5c3b99daae78e
bit2424 <nelsonquinones2424@gmail.com>
2024-05-03 18:43:27 +0000

    :white_check_mark: Tested the QueueObserver class.

---
+++ Backend/Transform/main.py
@@ -0,0 +1,45 @@
+from Core.QueueObserver import QueueObserver
+from Core.FilesProcessor import FilesProcessor
+import argparse
+import datetime
+import logging
+import time
+
+
+def main():
+  #Handling script arguments
+  parser = argparse.ArgumentParser(description="Queue Observer Script")
+  parser.add_argument("--folder", "-f", type=str, required=False, default="./../Transform_Queue/",
+                      help="Path to the folder to observe (default: ./../Transform_Queue/)")
+  args = parser.parse_args()
+
+  #Setting up logging system
+  now = datetime.datetime.now()
+  timestamp = now.strftime('%Y-%m-%d_%H-%M-%S')
+  filename = f'./Processing_Logs/transform_{timestamp}.log'
+  logging.basicConfig(filename=filename, filemode='w', format='%(asctime)s %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.INFO)
+  
+
+  try:
+    #Initializing the updater
+    files_processor = FilesProcessor(num_workers=4)
+    observer = QueueObserver(watch_dir=args.folder,files_processor=files_processor)
+    observer.start()
+    
+    # Keep the script running to monitor changes
+    while True:
+      observer.files_processor.update_time_to_process()
+      time.sleep(0.5)
+      
+  except Exception as e:
+    logger.exception("Exception occurred ", e)
+  except KeyboardInterrupt:
+    logging.info("Server Stop")
+      
+  logging.info("Server Shutdown")
+  observer.stop()
+
+if __name__ == "__main__":
+  main()
