1498dd49e8eb0a37f3ac2106a848860bf9c3bebe
bit2424 <nelsonquinones2424@gmail.com>
2024-05-21 08:56:28 +0000

    :white_check_mark: Improved multiprocessing tests

--- Backend/Tests/conftest.py
+++ Backend/Tests/conftest.py
@@ -22,21 +22,27 @@ def caplog_workaround():
     
     @contextmanager
     def ctx():
+        print("Created context")
         logger_queue = Queue()
         logger = logging.getLogger()
         logger.addHandler(handlers.QueueHandler(logger_queue))
         yield
+        print("Starting to end context")
         logger.removeHandler(handlers.QueueHandler(logger_queue))
         while not logger_queue.empty():
             log_record: logging.LogRecord = logger_queue.get()
+            print(log_record.message)
             logger._log(
                 level=log_record.levelno,
                 msg=log_record.message,
                 args=log_record.args,
                 exc_info=log_record.exc_info,
             )
+            # print("Not finished")
             if(log_record.message == STOP_SIGNAL):
+                print("Finished with stop signal")
                 break
-            
+        print("End of context")
+        
 
     return ctx
