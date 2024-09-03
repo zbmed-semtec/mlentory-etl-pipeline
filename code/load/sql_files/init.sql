08abfb5bb9f9628ecd15519041069b7ef7fe8354
bit2424 <nelsonquinones2424@gmail.com>
2024-09-03 14:41:06 +0000

    :sparkles::white_check_mark: Creating and updating triplets with ranges.

--- code/load/sql_files/init.sql
+++ code/load/sql_files/init.sql
@@ -3,23 +3,23 @@ USE MLentory_DB;
 
 CREATE TABLE IF NOT EXISTS `Version_Range`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `triplet_id` BIGINT NOT NULL,
+    `triplet_id` BIGINT UNSIGNED NOT NULL,
     `start` DATETIME NOT NULL,
     `end` DATETIME NOT NULL,
     `deprecated` BOOLEAN NOT NULL DEFAULT FALSE,
-    `extraction_info_id` BIGINT NOT NULL
+    `extraction_info_id` BIGINT UNSIGNED NOT NULL
 );
 CREATE TABLE IF NOT EXISTS `Triplet`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `subject` TEXT NOT NULL,
-    `relation` TEXT NOT NULL,
+    `subject` varchar(512) NOT NULL,
+    `predicate` varchar(512) NOT NULL,
     `object` TEXT NOT NULL,
-    UNIQUE KEY `unique_triplet` (`subject`(512), `relation`(512), `object`(512))
+    UNIQUE KEY `unique_triplet` (`subject`(512), `predicate`(512), `object`(512))
 );
 CREATE TABLE IF NOT EXISTS `Triplet_Extraction_Info`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `method_description` BIGINT NOT NULL,
-    `extraction_confidence` FLOAT NOT NULL
+    `method_description` TEXT NOT NULL,
+    `extraction_confidence` DECIMAL(6, 5)
 );
 
 ALTER TABLE
