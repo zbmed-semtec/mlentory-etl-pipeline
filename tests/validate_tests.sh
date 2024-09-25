8d5395f7afed4382b91f9eae39b5e63ee2276b53
bit2424 <nelsonquinones2424@gmail.com>
2024-09-25 14:07:23 +0000

    :construction_worker: Improving how to check the success of tests.

--- tests/validate_tests.sh
+++ tests/validate_tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-test_results=$(docker-compose --profile ci_test up --abort-on-container-exit)
-echo "RUNNING THE TESTS $test_results"
+docker-compose --profile=ci_test up -d
 
-last_three_lines=$(echo "$test_results" | tail -n 3)
+sleep 4
 
-if echo "$last_three_lines" | grep -i -q "error" > /dev/null || echo "$last_three_lines" | grep -i -q "errors" > /dev/null || echo "$last_three_lines" | grep -i -q "failed" > /dev/null; then
-  echo "Tests failed!"
-  exit 1
-else
+docker exec ci_test pytest ./tests/
+
+if [ $? -eq 0 ]  
+then
   echo "Tests successful!"
+  # docker-compose --profile ci_test down    
+  exit 0
+else
+  echo "Tests failed!"
+  # docker-compose --profile ci_test down    
+  exit 1
 fi
 
-docker-compose --profile ci_test down                                
+                            
