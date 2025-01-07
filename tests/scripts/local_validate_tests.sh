17cb14d1fae6f8cbe8e9f735472d902ff17714d7
bit2424 <nelsonquinones2424@gmail.com>
2025-01-07 10:51:55 +0000

    :construction_worker: Updated tests scripts for CI/CD run

--- tests/scripts/local_validate_tests.sh
+++ tests/scripts/local_validate_tests.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-docker-compose --profile=ci_test build
-docker-compose --profile=ci_test up -d
 
-./wait-for-it.sh mysql:3306
+# Get the absolute path of the script directory, regardless of where it's called from
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null )"
 
-docker exec ci_test pytest ./tests/
+cd "$SCRIPT_DIR/../config/docker"
+docker-compose --profile=test build
+docker-compose --profile=test up -d
 
-exit $?
+docker exec local_test pytest ./unit
 
-docker-compose --profile=ci_test down
+EXIT_CODE=$?
+
+docker-compose --profile=test down
+
+exit $EXIT_CODE
--- tests/scripts/validate_tests.sh
+++ tests/scripts/validate_tests.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-cd "$(dirname "$0")/../config/docker"
+
+# Get the absolute path of the script directory, regardless of where it's called from
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null )"
+
+cd "$SCRIPT_DIR/../config/docker"
 docker-compose --profile=ci_test up -d
 
-../scripts/wait-for-it.sh mysql:3306
+"$SCRIPT_DIR/wait-for-it.sh" mysql:3306
 
-docker exec ci_test pytest ./tests/
+docker exec ci_test pytest ./unit
 
-exit $? 
+EXIT_CODE=$?
+
+docker-compose --profile=ci_test down
+
+exit $EXIT_CODE
