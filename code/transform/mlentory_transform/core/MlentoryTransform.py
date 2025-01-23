e360f4d4bdcc55da72f94ddca7cc26964a495276
bit2424 <nelsonquinones2424@gmail.com>
2025-01-23 16:44:14 +0000

    :sparkles: Introduced an abstraction class to handle the transformation process of different sources.

--- code/transform/mlentory_transform/core/MlentoryTransform.py
+++ code/transform/mlentory_transform/core/MlentoryTransform.py
@@ -3,7 +3,8 @@ import pandas as pd
 from datetime import datetime
 from tqdm import tqdm
 import os
-from hf_transform.FieldProcessorHF import FieldProcessorHF
+from ..hf_transform.TransformHF import TransformHF
+from .KnoledgeGraphHandler import KnowledgeGraphHandler
 
 class MlentoryTransform:
     """
@@ -36,6 +37,7 @@ class MlentoryTransform:
         """
         self.processed_data = []
         self.current_sources = {}
+        self.kg_handler = KnowledgeGraphHandler()
 
     def transform_HF_models(self,
                             new_schema: pd.DataFrame,
@@ -67,30 +69,51 @@ class MlentoryTransform:
             ValueError: If save_output_in_json is True but output_dir is not provided
         """
         
-        fields_processor = FieldProcessorHF(new_schema, transformations)
+        transform_hf = TransformHF(new_schema, transformations)
+
+        transformed_df = transform_hf.transform_models(extracted_df)
         
-        if save_output_in_json and output_dir is None:
-            raise ValueError("output_dir must be provided when save_output is True")
-
-        processed_models = []
-
-        for row_num, row in tqdm(
-            extracted_df.iterrows(),
-            total=len(extracted_df),
-            desc="Transforming progress",
-        ):
-            model_data = fields_processor.process_row(row)
-            processed_models.append(model_data)
-
-        transformed_df = pd.DataFrame(list(processed_models))
+        self.print_detailed_dataframe(transformed_df)
         
         #Transform the dataframe to a knowledge graph
-        knowledge_graph = self.transform_df_to_knowledge_graph(transformed_df)
+        knowledge_graph = self.kg_handler.dataframe_to_graph_M4ML_schema(
+            df=transformed_df,
+            identifier_column="schema.org:name",
+            platform="HF"
+        )
         
         self.current_sources["HF_models"] = knowledge_graph
         
         return transformed_df
 
+    def print_detailed_dataframe(self, df: pd.DataFrame):
+        """
+        Print the detailed dataframe
+        """
+        print("\n**DATAFRAME**")
+        print("\nColumns:", df.columns.tolist())
+        print("\nShape:", df.shape)
+        print("\nSample Data:")
+        for col in df.columns:
+            print("--------------------------------------------")
+            print(f"\n{col}:")
+            for row in df[col]:
+                # Limit the text to 100 characters
+                if isinstance(row, list ):
+                    row_data = row[0]["data"]
+                    if isinstance(row_data, str):
+                        print(row_data[:100])
+                    else:
+                        print(row_data)
+                else:
+                    print(row)
+            print("--------------------------------------------")
+            print()
+        print("\nDataFrame Info:")
+        print(df.info())
+    
+    
+    
     def save_indiviual_sources(self, output_dir: str):
         """
         Save each transformed source in a separate file in json format.
---
+++ code/transform/mlentory_transform/core/__init__.py
@@ -0,0 +1,4 @@
+from .MlentoryTransform import MlentoryTransform
+from .KnoledgeGraphHandler import KnowledgeGraphHandler
+
+__all__ = ["MlentoryTransform", "KnowledgeGraphHandler"]
